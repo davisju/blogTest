f4f59044d51f0535de76e0f1d0f89a58b2e27bd2
junho <jhchang@invanix.co.kr>
2018-03-19 14:22:56 +0900

    first blog

--- blogTest/src/feed/admin.py
+++ blogTest/src/feed/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Ariticle, Comment, HashTag
 
+@admin.register(Ariticle, Comment, HashTag)
+class FeedAdmin(admin.ModelAdmin):
+    pass
 # Register your models here.
